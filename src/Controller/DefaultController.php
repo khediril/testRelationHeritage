a72e439ef3177b6efd24a22580bcb8ea9fcbb2f9
khediril <khediril@yahoo.fr>
2022-05-30 20:21:23 +0100

    test

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -33,4 +33,39 @@ class DefaultController extends AbstractController
             }
         ]);
     }
+     /**
+     * @Route("/ajoutv", name="ajoutv")
+     */
+    public function ajoutvideo()
+    {
+       $video = new Video();
+       $video->setTitle("tiltle 1111");
+       $video->setVideoPath("v1.mpeg4");
+       $this->entityManager->persist($video);
+       $this->entityManager->flush();
+       dd($video);
+       /* return $this->json($post->getComments(), 200, [], [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);*/
+    }
+     /**
+     * @Route("/ajoutp", name="ajoutp")
+     */
+    public function ajoutpost()
+    {
+       $post = new Post();
+       $post->setTitle("tiltle post");
+       $post->setContent("content post");
+      // $video->setVideoPath("v1.mpeg4");
+       $this->entityManager->persist($post);
+       $this->entityManager->flush();
+       dd($post);
+       /* return $this->json($post->getComments(), 200, [], [
+            'circular_reference_handler' => function ($object) {
+                return $object->getId();
+            }
+        ]);*/
+    }
 }
